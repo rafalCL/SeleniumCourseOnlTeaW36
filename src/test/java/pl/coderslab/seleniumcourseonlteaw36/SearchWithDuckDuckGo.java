18862a12f3e4fdc93c76fbc5b5aa0e3a8463b61c
edu <edu@mail.pl>
2023-04-16 14:53:01 +0200

    configure ChromeDriver path

--- src/test/java/pl/coderslab/seleniumcourseonlteaw36/SearchWithDuckDuckGo.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw36/SearchWithDuckDuckGo.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SearchWithDuckDuckGo {
     @Test
     public void searchWithDuckDuckGo() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://duckduckgo.com/");
         WebElement searchInput = driver.findElement(By.id("search_form_input_homepage")); // .var
