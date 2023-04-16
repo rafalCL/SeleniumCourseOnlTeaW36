d3179d610953d1f592f20fcb8286133900df8bb4
edu <edu@mail.pl>
2023-04-16 14:54:40 +0200

    configure implicit wait

--- src/test/java/pl/coderslab/seleniumcourseonlteaw36/SearchWithDuckDuckGo.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw36/SearchWithDuckDuckGo.java
@@ -7,11 +7,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class SearchWithDuckDuckGo {
     @Test
     public void searchWithDuckDuckGo() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
         driver.get("https://duckduckgo.com/");
         WebElement searchInput = driver.findElement(By.id("search_form_input_homepage")); // .var
         searchInput.sendKeys("W pustyni i w puszczy");
