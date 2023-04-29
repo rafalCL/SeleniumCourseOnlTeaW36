332e0759a347d2668eaf120f4b99338140675abb
edu <edu@mail.pl>
2023-04-29 15:58:34 +0200

    d5t1 page object hotel test refactoring example part 7 refactor HotelTestLabMainPage to use PageFactory

--- src/test/java/pl/coderslab/seleniumcourseonlteaw36/pageobject/HotelTestlabMainPage.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw36/pageobject/HotelTestlabMainPage.java
@@ -1,18 +1,19 @@
 package pl.coderslab.seleniumcourseonlteaw36.pageobject;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HotelTestlabMainPage {
-    private final WebDriver driver;
+    @FindBy(css = "ul.navbar-nav li a.user_login")
+    private WebElement signInBtn;
 
     public HotelTestlabMainPage(WebDriver driver) {
-        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void clickSignIn() {
-        WebElement signInBtn = driver.findElement(By.cssSelector("ul.navbar-nav li a.user_login"));
         signInBtn.click();
     }
 }
