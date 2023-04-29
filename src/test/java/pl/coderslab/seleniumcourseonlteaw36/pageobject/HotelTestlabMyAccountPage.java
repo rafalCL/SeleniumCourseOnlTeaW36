4e4d843072cd88f75c6eb6e7070f99bcd432cafa
edu <edu@mail.pl>
2023-04-29 16:01:59 +0200

    d5t1 page object hotel test refactoring example part 7 refactor HotelTestLabMyAccountPage to use PageFactory

--- src/test/java/pl/coderslab/seleniumcourseonlteaw36/pageobject/HotelTestlabMyAccountPage.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw36/pageobject/HotelTestlabMyAccountPage.java
@@ -1,18 +1,19 @@
 package pl.coderslab.seleniumcourseonlteaw36.pageobject;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HotelTestlabMyAccountPage {
-    private final WebDriver driver;
+    @FindBy(css = "p.alert.alert-success")
+    private WebElement confirmationPanel;
 
     public HotelTestlabMyAccountPage(WebDriver driver) {
-        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public boolean isAccountCreationConfirmationDisplayed() {
-        WebElement confirmationPanel = driver.findElement(By.cssSelector("p.alert.alert-success"));
         return confirmationPanel.getText().equals("Your account has been created.");
     }
 }
