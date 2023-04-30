a544db14d229bf0eb3235c6288c216aea8939c0c
edu <edu@mail.pl>
2023-04-30 15:02:45 +0200

    d6t2 cucumber example. parametrize step implementations

--- src/cucumber/features/zad1.qwant.phrase.search.feature
+++ src/cucumber/features/zad1.qwant.phrase.search.feature
@@ -1,6 +1,6 @@
 Feature: Qwant phrase search
 Scenario: Successful phrase search
-Given Page https://www.qwant.com opened in browser
-When Phrase 'Szkice piórkiem' entered in search input box
+Given Page "https://www.qwant.com" opened in browser
+When Phrase "Szkice piórkiem" entered in search input box
 And Search button clicked
-Then First 3 results link texts contain phrase 'szkice piórkiem'
+Then First 3 results link texts contain phrase "szkice piórkiem"
--- src/test/java/pl/coderslab/seleniumcourseonlteaw36/pageobject/qwant/cucumber/QwantSearchCucumberSteps.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw36/pageobject/qwant/cucumber/QwantSearchCucumberSteps.java
@@ -15,18 +15,18 @@ import java.util.List;
 public class QwantSearchCucumberSteps {
     private WebDriver driver;
 
-    @Given("Page https:\\/\\/www.qwant.com opened in browser")
-    public void page_https_www_qwant_com_opened_in_browser() {
+    @Given("Page {string} opened in browser")
+    public void page_https_www_qwant_com_opened_in_browser(String pageUrl) {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         this.driver = new ChromeDriver();
         this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
-        this.driver.get("https://www.qwant.com/");
+        this.driver.get(pageUrl);
     }
 
-    @When("Phrase 'Szkice piórkiem' entered in search input box")
-    public void phrase_entered_in_search_input_box() {
+    @When("Phrase {string} entered in search input box")
+    public void phrase_entered_in_search_input_box(String searchPhrase) {
         QwantMainPage mainPage = new QwantMainPage(driver);
-        mainPage.enterSearchPhrase("Szkice piórkiem");
+        mainPage.enterSearchPhrase(searchPhrase);
     }
 
     @When("Search button clicked")
@@ -35,12 +35,12 @@ public class QwantSearchCucumberSteps {
         mainPage.clickSearchButton();
     }
 
-    @Then("First 3 results link texts contain phrase 'szkice piórkiem'")
-    public void first_results_link_texts_contain_phrase() {
+    @Then("First {int} results link texts contain phrase {string}")
+    public void first_results_link_texts_contain_phrase(int count, String searchPhrase) {
         QwantSearchResultPage resultPage = new QwantSearchResultPage(driver);
         List<String> results = resultPage.getResultsLinkTexts();
-        String phraseToContain = "szkice piórkiem";
-        for (int i = 0; i < 3; i++) {
+        String phraseToContain = searchPhrase.toLowerCase();
+        for (int i = 0; i < count; i++) {
             String resultText = results.get(i);
             if (!resultText.toLowerCase().contains(phraseToContain)) {
                 Assertions.fail("Search result must contain (case insensitive): " + phraseToContain + ", but was: " + resultText);
